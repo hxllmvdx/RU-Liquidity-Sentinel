fff497a656c81690fcd25e3e996efeb2aacb49b2
Базанова Виктория <vabazanova@mai.education>
2026-05-11 14:04:12 +0300

    New merge by days

--- ml-services/modules/m1_reserves/count_signal.py
+++ ml-services/modules/m1_reserves/count_signal.py
@@ -10,14 +10,14 @@ class M1CountSignals():
     
     def mad_score_ruonia(self):
         df= pd.DataFrame()
-        df["ruonia_mediana_day"] = self.df_all["ruonia"].rolling(window=1095, min_periods=365).median()
-        df["ruonia_mad_day"] = self.df_all["ruonia"].rolling(window=1095, min_periods=365).apply(lambda x: median_abs_deviation(x, nan_policy='omit'))
+        df["ruonia_mediana_day"] = self.df_all["ruonia"].rolling(window=1095, min_periods=50).median()
+        df["ruonia_mad_day"] = self.df_all["ruonia"].rolling(window=1095, min_periods=50).apply(lambda x: median_abs_deviation(x, nan_policy='omit'))
         self.df_all["mad_score_ruonia"] =(self.df_all["ruonia"] - df["ruonia_mediana_day"]) / df["ruonia_mad_day"]
 
     def mad_score_spread(self):
         df = pd.DataFrame()
-        df['spread_median'] = self.df_all["spread"].rolling(window=1095, min_periods=365).median()
-        df['spread_mad'] = self.df_all["spread"].rolling(window=1095, min_periods=365).apply(lambda x: median_abs_deviation(x, nan_policy='omit'))
+        df['spread_median'] = self.df_all["spread"].rolling(window=1095, min_periods=50).median()
+        df['spread_mad'] = self.df_all["spread"].rolling(window=1095, min_periods=50).apply(lambda x: median_abs_deviation(x, nan_policy='omit'))
         self.df_all['mad_score_spread'] = (self.df_all["spread"] - df['spread_median']) / df['spread_mad']
 
 
